746da5e057d003e693a1a5920b35165059a8b6fd
Saulo Filho <Saulo.Filho@Kinross.com>
2022-01-26 16:13:47 -0300

    Config server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/client-romm'));
+app.use(express.static('./dist/client-room'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/client-romm/'}),
+    res.sendFile('index.html', {root: 'dist/client-room/'}),
 );
 
 // Start the app by listening on the default Heroku port
